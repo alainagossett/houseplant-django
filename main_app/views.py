2be4ec3bcd601e771e49ee042e22d24e9d92f31b
Alaina <alaina.gossett@gmail.com>
2022-02-16 19:56:44 -0700

    adds plant model and index

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+class Plant:
+    def __init__(self, name, description, sunlight, water, issues):
+        self.name = name
+        self.description = description
+        self.sunlight = sunlight
+        self.water = water
+        self.issues = issues
+
+plants = [
+    Plant('Monstera Deliciosa', 
+    'Famous for their natural leaf-holes, this popular plant is fondly nicknamed the Swiss Cheese Plant.',
+    'Bright to medium indirect light',
+    'Every 1-2 weeks, depending on light level',
+    'Fungus flies, root rot, underwatering')
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def plants_index(request):
+    return render(request, 'plants/index.html', { 'plants': plants })
