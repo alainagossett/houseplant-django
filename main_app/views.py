46bae79f9c505ca8eb3204153706ccce40a89164
Alaina <alaina.gossett@gmail.com>
2022-02-16 20:07:32 -0700

    adds Plant model

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,5 @@
 from django.shortcuts import render
-
-class Plant:
-    def __init__(self, name, description, sunlight, water, issues):
-        self.name = name
-        self.description = description
-        self.sunlight = sunlight
-        self.water = water
-        self.issues = issues
-
-plants = [
-    Plant('Monstera Deliciosa', 
-    'Famous for their natural leaf-holes, this popular plant is fondly nicknamed the Swiss Cheese Plant.',
-    'Bright to medium indirect light',
-    'Every 1-2 weeks, depending on light level',
-    'Fungus flies, root rot, underwatering')
-]
+from .models import Plant
 
 # Create your views here.
 def home(request):
@@ -24,4 +9,5 @@ def about(request):
     return render(request, 'about.html')
 
 def plants_index(request):
+    plants = Plant.objects.all()
     return render(request, 'plants/index.html', { 'plants': plants })
