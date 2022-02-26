7611218e52442a605017e304153e1e87e43f4dc8
Alaina <alaina.gossett@gmail.com>
2022-02-26 12:44:07 -0700

    adds watering logic

--- main_app/static/js/script.js
+++ main_app/static/js/script.js
@@ -7,6 +7,16 @@ M.Datepicker.init(dateEl, {
     setDefaultDate: true,
     autoClose: true
 });
+// document.addEventListener('DOMcontentLoaded', function() {
+//     var dateEl = document.querySelectorAll('.datepicker');
+//     var date = M.Datepicker.init(dateEl, {
+//         format: 'yyyy-mm-dd',
+//         defaultDate: new Date(),
+//         setDefaultDate: true,
+//         autoClose: true
+//     });
+// })
+
 
 const checkedEl = document.getElementById('id_watered');
 
