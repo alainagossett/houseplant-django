bf34235b5fb9b174485eb62aff32bf3d7881a387
Alaina <alaina.gossett@gmail.com>
2022-02-28 19:21:04 -0700

    adds photo model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from datetime import date
 
 # Create your models here.
 class Accessory(models.Model):
@@ -40,3 +39,11 @@ class Watering(models.Model):
     
     class Meta:
         ordering = ('-date',)
+
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for plant_id: {self.plant_id} @ {self.url}"
